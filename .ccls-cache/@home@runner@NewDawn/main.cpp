4fdd2de217ac0e45d9d1fcc7950402bfa322d92e
SidParulekar <76064146+SidParulekar@users.noreply.github.com>
2024-05-19 09:40:11 +0000

    Test

--- .ccls-cache/@home@runner@NewDawn/main.cpp
+++ .ccls-cache/@home@runner@NewDawn/main.cpp
@@ -152,6 +152,7 @@ class Enemy
     cout<<"Umbra's Health: "<<health<<endl;
     cout<<"Umbra's normal hit attack power is between "<<max_damage<<" and "<<min_damage<<endl;
     cout<<"Umbra's's critical hit Attack Power: "<<umbra_critical_hit<<endl;
+    cout<<"Umbra's's critical hit Attack Power: "<<umbra_critical_hit<<endl;
     cout<<"\n<-------------------------------------------------------------->"<<endl;
 
   }
